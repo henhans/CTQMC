0be68286aeb5fea012a2f51dd9b2f5f54d3e332e
Tsung-Han Lee <henhans74716@gmail.com>
2016-01-08 15:44:04 -0500

    waiting for construction... using Eigen determinant first for testing

--- det.h
+++ det.h
@@ -1,3 +1,22 @@
-class Det {
+#include <eigen3/Eigen/Dense>
 
-}
+using namespace Eigen;
+
+class Det {
+    // class for hybridization matrix delta and its inverse M, and the 
+    // routine that calculates the determinant.
+    int flavor;
+    vector<Matrix2d> M;//Matrix M for inverse hybnridization matrix
+
+    public:
+    // initialize the determinant
+    Det(int flavor_) {
+        flavor = flavor_;
+        for( int i=0; i<flavor; i++){
+            clog << "setup matrix M for flavor:" << i << endl;
+            Matrix2d tmp;
+            M.push_back(tmp);
+        };
+    };
+
+};
