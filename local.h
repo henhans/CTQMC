0e6f8e817fab44c0f727092d85fbca8092f6312d
Tsung-Han Lee <henhans74716@gmail.com>
2016-01-13 11:52:00 -0500

    implemented local sign change ratio

--- local.h
+++ local.h
@@ -5,7 +5,9 @@ class Local{
     double U; // interaction
     double ed;// impurity level
     double beta; // inverse temperature
-     
+    int sign_old; // sign for the last segment -1 if it wrap back to the front, 1 if it doesn't wrap back
+    int sign;
+
     public:
     // initial local class
     Local(int flavor_, double U_, double ed_, double beta_){
@@ -14,5 +16,21 @@ class Local{
         ed = ed_;
         beta = beta_;
         clog << "flavor="<< flavor << " U=" << U <<" ed=" << ed <<" beta=" << beta << endl;
+        sign_old = 1;// initialize to 1 cause the empty configuration has sign 1
     };
+
+    double calc_local_trace(Time_config& t_config_);
+    void update_trace();
+};
+
+double Local::calc_local_trace(Time_config& t_config_){
+    int size = t_config_.get_pertur_order(0);
+    if(t_config_.get_t_start_at(0,size-1) > t_config_.get_t_end_at(0,size-1) ) sign = -1;
+    else sign = 1;
+
+    return sign*sign_old;
+};
+
+void Local::update_trace() {
+    sign_old = sign;
 };
