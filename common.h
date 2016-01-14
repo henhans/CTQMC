b8e0e4923d6ba56f09581ec26332a23608954105
Tsung-Han Lee <henhans74716@gmail.com>
2016-01-14 17:07:51 -0500

    added new parameters

--- common.h
+++ common.h
@@ -5,21 +5,23 @@
      static int max_steps;
      static double U;    
      static double ed;
-     static double t;
+     static double V;
      static double beta;
      static double minM;// The smallest Ddeterminant to accept in order to prevent singularity.
      static double minD;// The smallest Ddeterminant to accept in order to prevent singularity.
- 
-     static void set_params(int flavor_, int max_steps_ ,double U_, double ed_, double t_, double beta_, double minM_,
-     double minD_) {
+     static double warmup;
+
+     static void set_params(int flavor_, int max_steps_ ,double U_, double ed_, double V_, double beta_, double minM_,
+     double minD_, int warmup_) {
          flavor = flavor_;
          max_steps = max_steps_;
          U = U_;
          ed = ed_;
-         t = t_;
+         V = V_;
          beta = beta_;
          minM = minM_;
          minD = minD_;
+         warmup = warmup_;
      };
  };
 
